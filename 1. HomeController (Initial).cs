23507c36f1a9923fc8e6a1a7b8d87c4c412744f7
Max Garanin <maxgaranin@gmail.com>
2020-12-18 12:38:42 +0400

    feat: добавил пример борьбы с уязвимостью CSRF

---
+++ 1. HomeController (Initial).cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebAuthSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize(Roles = "admin, user")]
+        public IActionResult Index()
+        {
+            string role = User.FindFirst(x => x.Type == ClaimsIdentity.DefaultRoleClaimType).Value;
+            return Content($"Ваша роль: {role}");
+        }
+
+        [Authorize(Roles = "admin")]
+        public IActionResult Admin()
+        {
+            return Content("Вход только для администратора");
+        }
+    }
+}
